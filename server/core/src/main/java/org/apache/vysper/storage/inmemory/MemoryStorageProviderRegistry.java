24e6e0435120f36f850b254a53f77a5a10c41f51
Michael Jakl <mjakl@apache.org>
2009-08-05 12:22:12 +0000

    Corrected spelling of CollectionNodeInMemoryStorageProvider.
    git-svn-id: https://svn.apache.org/repos/asf/mina/sandbox/vysper/trunk@801170 13f79535-47bb-0310-9956-ffa450edef68

--- server/core/src/main/java/org/apache/vysper/storage/inmemory/MemoryStorageProviderRegistry.java
+++ server/core/src/main/java/org/apache/vysper/storage/inmemory/MemoryStorageProviderRegistry.java
@@ -36,7 +36,7 @@ public class MemoryStorageProviderRegistry extends OpenStorageProviderRegistry {
         
         // provider from external modules, low coupling, fail when modules are not present
         add("org.apache.vysper.xmpp.modules.extension.xep0060_pubsub.storageprovider.LeafNodeInMemoryStorageProvider");
-        add("org.apache.vysper.xmpp.modules.extension.xep0060_pubsub.storageprovider.CollectionnodeInMemoryStorageProvider");
+        add("org.apache.vysper.xmpp.modules.extension.xep0060_pubsub.storageprovider.CollectionNodeInMemoryStorageProvider");
     }
 
 }
