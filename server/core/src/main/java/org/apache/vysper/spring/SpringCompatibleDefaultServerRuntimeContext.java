683cab8db08c1887bd46ff2d9d89965a860ea49c
Bernd Fondermann <berndf@apache.org>
2009-09-03 11:09:29 +0000

    fix circular dependencies in Spring bean declaration by deferring initializion stanza relay from constructor-injection to setter injection
    git-svn-id: https://svn.apache.org/repos/asf/mina/sandbox/vysper/trunk@810891 13f79535-47bb-0310-9956-ffa450edef68

--- server/core/src/main/java/org/apache/vysper/spring/SpringCompatibleDefaultServerRuntimeContext.java
+++ server/core/src/main/java/org/apache/vysper/spring/SpringCompatibleDefaultServerRuntimeContext.java
@@ -1,29 +1,41 @@
 package org.apache.vysper.spring;
 
-import org.apache.vysper.xmpp.server.DefaultServerRuntimeContext;
-import org.apache.vysper.xmpp.server.ServerFeatures;
 import org.apache.vysper.xmpp.addressing.Entity;
 import org.apache.vysper.xmpp.delivery.StanzaRelay;
-import org.apache.vysper.xmpp.protocol.NamespaceHandlerDictionary;
-import org.apache.vysper.xmpp.state.resourcebinding.ResourceRegistry;
+import org.apache.vysper.xmpp.delivery.failure.DeliveryException;
+import org.apache.vysper.xmpp.delivery.failure.DeliveryFailureStrategy;
 import org.apache.vysper.xmpp.modules.Module;
-import org.apache.vysper.storage.StorageProviderRegistry;
+import org.apache.vysper.xmpp.protocol.NamespaceHandlerDictionary;
+import org.apache.vysper.xmpp.server.DefaultServerRuntimeContext;
+import org.apache.vysper.xmpp.server.ServerFeatures;
+import org.apache.vysper.xmpp.stanza.Stanza;
+import org.apache.vysper.xmpp.state.resourcebinding.ResourceRegistry;
 
 import java.util.List;
 
 /**
  */
 public class SpringCompatibleDefaultServerRuntimeContext extends DefaultServerRuntimeContext {
-    public SpringCompatibleDefaultServerRuntimeContext(Entity serverEntity, StanzaRelay stanzaRelay) {
-        super(serverEntity, stanzaRelay);
-    }
+    
+    private static class StanzaRelayHull implements StanzaRelay {
+        
+        protected StanzaRelay stanzaRelay;
 
-    public SpringCompatibleDefaultServerRuntimeContext(Entity serverEntity, StanzaRelay stanzaRelay, StorageProviderRegistry storageProviderRegistry) {
-        super(serverEntity, stanzaRelay, storageProviderRegistry);
-    }
+        public void setStanzaRelay(StanzaRelay stanzaRelay) {
+            this.stanzaRelay = stanzaRelay;
+        }
 
-    public SpringCompatibleDefaultServerRuntimeContext(Entity serverEntity, StanzaRelay stanzaRelay, ServerFeatures serverFeatures, List<NamespaceHandlerDictionary> dictionaries, ResourceRegistry resourceRegistry) {
-        super(serverEntity, stanzaRelay, serverFeatures, dictionaries, resourceRegistry);
+        public void relay(Entity receiver, Stanza stanza, DeliveryFailureStrategy deliveryFailureStrategy) throws DeliveryException {
+            stanzaRelay.relay(receiver, stanza, deliveryFailureStrategy);
+        }
+    }
+    
+    public SpringCompatibleDefaultServerRuntimeContext(Entity serverEntity, ServerFeatures serverFeatures, List<NamespaceHandlerDictionary> dictionaries, ResourceRegistry resourceRegistry) {
+        super(serverEntity, new StanzaRelayHull(), serverFeatures, dictionaries, resourceRegistry);
+    }
+    
+    public void setStanzaRelay(StanzaRelay stanzaRelay) {
+        ((StanzaRelayHull)getStanzaRelay()).setStanzaRelay(stanzaRelay);
     }
 
     public void setModules(List<Module> modules) {
