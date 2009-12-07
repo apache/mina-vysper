f8c8e06826eeac1263776dde5f53137d3488ffc5
Niklas Gustavsson <ngn@apache.org>
2009-12-07 17:52:09 +0000

    Renamed packaged for XML implementation
    git-svn-id: https://svn.apache.org/repos/asf/mina/sandbox/vysper/trunk@888049 13f79535-47bb-0310-9956-ffa450edef68

---
+++ nbxml/src/main/java/org/apache/vysper/xml/fragment/EqualityVerifier.java
@@ -0,0 +1,92 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.vysper.xml.fragment;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.List;
+
+/**
+ *
+ * @author The Apache MINA Project (dev@mina.apache.org)
+ */
+public class EqualityVerifier {
+
+    private static AttributeComparator ATTRIBUTE_COMPARATOR = new AttributeComparator();
+
+    /**
+     * checks only name, namespace and attributes of both XMLElements for equality,
+     * but not inner elements
+     */
+    public boolean equalsElements_Shallow(XMLElement first, XMLElement second) {
+        if (first == null) return second == null;
+        if (second == null) return false;
+
+        if (!first.getName().equals(second.getName())) return false;
+        if (!first.getNamespacePrefix().equals(second.getNamespacePrefix())) return false;
+
+        return equalsAttributes(first.getAttributes(), second.getAttributes());
+    }
+
+    /**
+     * additional to the shallow check, does check the inner elements. more precisely, it checks whether
+     * all inner elements at corresponding positions are equal. it does _not_ check whether inner elements
+     * are the same for some proper mutation.
+     */
+    public boolean equalsElements_DeepSameOrder(XMLElement first, XMLElement second) {
+        if (first == null) return second == null;
+        if (second == null) return false;
+
+        if (!first.getName().equals(second.getName())) return false;
+        if (!first.getNamespacePrefix().equals(second.getNamespacePrefix())) return false;
+
+        boolean attrEquals = equalsAttributes(first.getAttributes(), second.getAttributes());
+        if (!attrEquals) return false;
+
+        List<XMLElement> firstInnerElements = first.getInnerElements();
+        List<XMLElement> secondInnerElements = second.getInnerElements();
+
+        if (firstInnerElements.size() != secondInnerElements.size()) return false;
+        for (int i = 0; i < firstInnerElements.size(); i++) {
+            if (!equalsElements_DeepSameOrder(firstInnerElements.get(0), secondInnerElements.get(0))) return false;
+        }
+        return true;
+    }
+
+    private boolean equalsAttributes(List<Attribute> firstAttrs, List<Attribute> secondAttrs) {
+        TreeSet<Attribute> attrSorted1 = new TreeSet<Attribute>(ATTRIBUTE_COMPARATOR);
+        attrSorted1.addAll(firstAttrs);
+
+        TreeSet<Attribute> attrSorted2 = new TreeSet<Attribute>(ATTRIBUTE_COMPARATOR);
+        attrSorted2.addAll(secondAttrs);
+
+        return attrSorted1.equals(attrSorted2);
+    }
+
+    static class AttributeComparator implements Comparator<Attribute> {
+
+        public int compare(Attribute attribute1, Attribute attribute2) {
+            if (attribute1.getName().equals(attribute2.getName())) {
+                return attribute1.getValue().compareTo(attribute2.getValue());
+            }
+            return attribute1.getName().compareTo(attribute2.getName());
+        }
+    }
+}
---
+++ nbxml/src/test/java/org/apache/vysper/xml/decoder/XMLRawToFragmentConverterConvertErrorTestCase.java
@@ -0,0 +1,277 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+package org.apache.vysper.xml.decoder;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.vysper.xml.decoder.DecodingException;
+import org.apache.vysper.xml.decoder.UnsupportedXMLException;
+import org.apache.vysper.xml.decoder.XMLParticle;
+import org.apache.vysper.xml.decoder.XMLRawToFragmentConverter;
+
+/**
+ */
+public class XMLRawToFragmentConverterConvertErrorTestCase extends TestCase {
+    private XMLRawToFragmentConverter xmlRawToFragmentConverter;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        xmlRawToFragmentConverter = new XMLRawToFragmentConverter();
+    }
+
+    public void testTwoToplevels() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<balanced>"));
+        particles.add(new XMLParticle("</balanced>"));
+        particles.add(new XMLParticle("<illegal-top-level>"));
+        particles.add(new XMLParticle("</illegal-top-level>"));
+
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("to consecutive elements are not allowed on top level");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testTwoToplevels2() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<balanced>"));
+        particles.add(new XMLParticle("<inner />"));
+        particles.add(new XMLParticle("</balanced>"));
+        particles.add(new XMLParticle("<illegal-top-level>"));
+        particles.add(new XMLParticle("</illegal-top-level>"));
+
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("to consecutive elements are not allowed on top level");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testIllegalOpening() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<start <illegal start>> "));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("opening not allowed inside element");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testIllegalOpeningAfterAttr() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<start attr='val' <illegal start>>"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("opening not allowed inside element");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testLateOpeningElementName() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("< latename attr='val'>"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("opening xml element name to follow immediately after '<' ");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testWrongClosingElementName() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr='val'>"));
+        particles.add(new XMLParticle("</nameother >"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("closing xml element name to follow immediately after '</'");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testWrongClosingElementWhitespace() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr='val'>"));
+        particles.add(new XMLParticle("</ name >"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("closing xml element name to follow immediately after '</'");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testWrongClosingElementWhitespace2() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr='val'>"));
+        particles.add(new XMLParticle("< /name >"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("closing xml element must start with '</'");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testStartElementEmpty() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        XMLParticle particle = new XMLParticle("< />");
+        try {
+            String name = particle.getElementName();
+            fail("closing xml element must not be empty (direct access)");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+        particles.add(particle);
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("closing xml element must not be empty (indirect access)");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testElementNameColon_Only() throws DecodingException {
+
+        XMLParticle particle = new XMLParticle("<: />");
+        try {
+            String name = particle.getElementName();
+            fail("illegal element name ':'");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testElementNameColon_NoName() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        XMLParticle particle = new XMLParticle("<namespace: />");
+        particles.add(particle);
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("illegal element name");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testMoreEmptyElements() throws DecodingException {
+        try {
+            String name = new XMLParticle("<>").getElementName();
+            fail("xml element name is mandatory");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+
+        try {
+            String name = new XMLParticle("< >").getElementName();
+            fail("xml element name is mandatory");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+
+        try {
+            String name = new XMLParticle("< attr='k' >").getElementName();
+            fail("xml element name is mandatory");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+
+        try {
+            String name = new XMLParticle("< attr='k' ></>").getElementName();
+            fail("xml element name is mandatory");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testWrongOpeningClosingElementWhitespace2() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr='val' / >"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("opening+closing xml element must end with '/>'");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+
+    public void testAttributeKeyIsNotFollowedByEquals() throws DecodingException {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr 'val' />"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("attribute and value must be separated by '='");
+        } catch (DecodingException e) {
+            // test succeded
+        }
+    }
+    
+    public void testUnsupportedXMLComment() {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<!-- unsupported comment -->"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("comments are unsupported");
+        } catch (UnsupportedXMLException e) {
+            // test succeded
+        } catch (DecodingException e) {
+            fail("more generic exception as expected was thrown");
+        }
+    }
+    
+    public void testUnsupportedXMLCommentNested() {
+        List<XMLParticle> particles = new ArrayList<XMLParticle>();
+
+        particles.add(new XMLParticle("<name attr='val'>"));
+        particles.add(new XMLParticle("<!-- unsupported comment -->"));
+        particles.add(new XMLParticle("</name >"));
+        try {
+            xmlRawToFragmentConverter.convert(particles);
+            fail("comments are unsupported");
+        } catch (UnsupportedXMLException e) {
+            // test succeded
+        } catch (DecodingException e) {
+            fail("more generic exception as expected was thrown");
+        }
+    }
+}
