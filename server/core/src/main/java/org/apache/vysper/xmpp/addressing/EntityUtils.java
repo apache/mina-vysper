b37e691c8e842a5e9e5d1dc82a03fd056ed002d9
Bernd Fondermann <berndf@apache.org>
2010-08-27 22:18:35 +0000

    VYSPER-246: javadoc EntityUtils
    git-svn-id: https://svn.apache.org/repos/asf/mina/vysper/trunk@990287 13f79535-47bb-0310-9956-ffa450edef68

--- server/core/src/main/java/org/apache/vysper/xmpp/addressing/EntityUtils.java
+++ server/core/src/main/java/org/apache/vysper/xmpp/addressing/EntityUtils.java
@@ -7,14 +7,32 @@ import org.apache.vysper.xmpp.server.ServerRuntimeContext;
  */
 public class EntityUtils {
 
+    /**
+     * checks a JID to be addressed to the server's domain
+     * @param toVerify - JID to be verified 
+     * @param serverJID - JID of the server
+     * @return TRUE iff toVerify JID equals the server's JID
+     */
     public static boolean isAddressingServer(Entity toVerify, Entity serverJID) {
         return toVerify.getDomain().equals(serverJID.getDomain());
     }
     
+    /**
+     * checks a JID to be addressed to any of the server's component
+     * @param toVerify - JID to be verified 
+     * @param serverJID - JID of the server
+     * @return TRUE iff toVerify's domain is a component of the given server JID
+     */
     public static boolean isAddressingServerComponent(Entity toVerify, Entity serverJID) {
         return toVerify.getDomain().endsWith("." + serverJID.getDomain());
     }
 
+    /**
+     * creates a JID with only a domain part set, the domain part being assembled from a subdomain and the server domain.
+     * @param subdomain
+     * @param serverRuntimeContext to retrieve the server JID
+     * @return subdomain.serverdomain.tld
+     */
     public static Entity createComponentDomain(String subdomain, ServerRuntimeContext serverRuntimeContext) {
         try {
             return EntityImpl.parse(subdomain + "." + serverRuntimeContext.getServerEnitity().getDomain());
