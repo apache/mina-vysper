0db999f0b8cbbcb33e1d00ee5e976448e231355e
Bernd Fondermann <berndf@apache.org>
2012-10-14 09:24:24 +0000

    optimize imports
    git-svn-id: https://svn.apache.org/repos/asf/mina/vysper/trunk@1398041 13f79535-47bb-0310-9956-ffa450edef68

--- server/extensions/xep0045-muc/src/main/java/org/apache/vysper/xmpp/modules/extension/xep0045_muc/model/Room.java
+++ server/extensions/xep0045-muc/src/main/java/org/apache/vysper/xmpp/modules/extension/xep0045_muc/model/Room.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.vysper.xmpp.addressing.Entity;
 import org.apache.vysper.xmpp.addressing.EntityImpl;
